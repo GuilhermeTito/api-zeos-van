2cb1100860458bdfc6b98bd8104e9a8e5f833497
Guilherme Tito <guilhermeferrazronha123@gmail.com>
2024-10-24 00:27:34 -0300

    feat: adicionados campos de pontos e horários padrão para passageiro

--- model/passageiro.js
+++ model/passageiro.js
@@ -4,39 +4,51 @@ const { DataTypes, Model } = require("sequelize")
 class Passageiro extends Model {}
 
 Passageiro.init(
-  {
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        telefone: {
+            type: DataTypes.STRING
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        data_cadastro: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        ponto_partida_padrao: {
+            type: DataTypes.STRING,
+        },
+        horario_partida_padrao:{
+            type: DataTypes.TIME
+        },
+        ponto_chegada_padrao: {
+            type: DataTypes.STRING
+        },
+        horario_chegada_padrao: {
+            type: DataTypes.TIME
+        }
     },
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    telefone: {
-      type: DataTypes.STRING
-    },
-    senha: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    data_cadastro: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false
+    {
+        sequelize: db,
+        modelName: "passageiro"
     }
-  },
-  {
-    sequelize: db,
-    modelName: "passageiro"
-  }
 );
 
 module.exports = Passageiro
